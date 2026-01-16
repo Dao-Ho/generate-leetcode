02ea23ffc880727047668b7d73047fc61e8772ff
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-01-16 03:17:08 -0500

    config struct + env package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"context"
+	"log"
+
+	"generate-leetcode/config"
+	"github.com/sethvargo/go-envconfig"
+)
+
+func main() {
+	var cfg config.Config
+	ctx := context.Background()
+	if err := envconfig.Process(ctx, &cfg); err != nil {
+		log.Fatal("failed to process config:", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.25.4
 
 require (
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/sethvargo/go-envconfig v1.3.0 // indirect
 	github.com/slack-go/slack v0.17.3 // indirect
 )
